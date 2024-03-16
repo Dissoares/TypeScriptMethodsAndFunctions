e564fea2f303beb0d38cfe57ee9b55c55d01fbe0
Dissoares <diegosoaresyt@gmail.com>
2024-03-16 17:36:30 -0300

    usando abordagem simples sem switchcase

--- SwitchCaseComListaDeObjetos.js
+++ SwitchCaseComListaDeObjetos.js
@@ -11,17 +11,19 @@ let diasSemana = [
   { id: 7, dia: "Sábado" },
 ];
 
-if (diaAtual == 0 || diaAtual > diasSemana.length) {
-  console.log("O número: "+ diaAtual +" não eh válido, digite um número de 1 a 7 correspondente ao dia da semana");
-} else {
-  switch (diaAtual) {
-   case 1: case 2: case 3: case 4: case 5: case 6: case 7: 
-      resultado = diasSemana[diaAtual-1].dia;
-      break;
+// if (diaAtual == 0 || diaAtual > diasSemana.length) {
+//   console.log("O número: "+ diaAtual +" não eh válido, digite um número de 1 a 7 correspondente ao dia da semana");
+// } else {
+//   switch (diaAtual) {
+//    case 1: case 2: case 3: case 4: case 5: case 6: case 7: 
+//       resultado = diasSemana[diaAtual-1].dia;
+//       break;
 
-    default:
-      return  console.log("o tipo '"+ typeof diaAtual +"' com o valor: {"+ diaAtual+"} não eh vádido!:")
-  }
-}
+//     default:
+//       return  console.log("o tipo '"+ typeof diaAtual +"' com o valor: {"+ diaAtual+"} não eh vádido!:")
+//   }
+// }
+
+resultado = diasSemana[diaAtual-1].dia; // usando abordagem simples sem switchcase
 
 console.log(resultado);
