5376f0b962abae5153d91985fbe16ae8d4229620
Dissoares <diegosoaresyt@gmail.com>
2024-03-31 19:02:51 -0300

    Manipulando arrays

---
+++ ManipulandoElementosNumArray.js
@@ -0,0 +1,11 @@
+var number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+
+number.push(11) //!adiciona um elemento a ultima posição do array
+
+number.pop(11) //!remove o ultimo elemento do array
+
+number.unshift('intens', 25) //!adiciona um elemento a primeira posição do array
+
+console.log("🚀 ~ tamanho:", number.length)
+console.log("🚀 ~ itens:", number)
